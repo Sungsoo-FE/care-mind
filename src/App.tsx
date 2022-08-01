afcba6220f4489332f05f8a9ad6356bc2c3c7415
kwtjdtn <kwtjdtn1@naver.com>
2022-08-01 11:04:30 +0900

    feat: font size type 선언 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import Description from "./components/description";
 import NavigationBar from "./components/navigationBar";
 
 function App() {
-  const [changedFontSize, setChangedFontSize] = useState(0);
+  const [changedFontSize, setChangedFontSize] = useState<Number>(0);
 
   return (
     <div>
       <header className="App-header">
         <NavigationBar changedFontSize={changedFontSize} />
         <div style={{ paddingLeft: 12, paddingRight: 12 }}>
-          <text style={{ fontSize: 12 + changedFontSize }}>
-            current font size is {changedFontSize + 12}
+          <text style={{ fontSize: 12 + Number(changedFontSize) }}>
+            current font size is {Number(changedFontSize) + 12}
           </text>
           <Description changedFontSize={changedFontSize} />
           <div
@@ -22,10 +22,14 @@ function App() {
               justifyContent: "space-between",
             }}
           >
-            <button onClick={() => setChangedFontSize(changedFontSize + 1)}>
+            <button
+              onClick={() => setChangedFontSize(Number(changedFontSize) + 1)}
+            >
               +
             </button>
-            <button onClick={() => setChangedFontSize(changedFontSize - 1)}>
+            <button
+              onClick={() => setChangedFontSize(Number(changedFontSize) - 1)}
+            >
               -
             </button>
           </div>
